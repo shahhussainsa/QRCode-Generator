d18dce37ffcbf0de5f9c2a34f23b783da03fe3e9
SHAH HUSSAIN AHAMED S A <112507587+shahhussainsa@users.noreply.github.com>
2024-06-09 09:18:11 +0530

    Update QRcode.py

--- QRcode.py
+++ QRcode.py
@@ -5,7 +5,7 @@ qr = qrcode.QRCode(version = 1,
                    error_correction = qrcode.constants.ERROR_CORRECT_H,
                    box_size=10, border=4,)
 
-qr.add_data("https://shahhussainsa.github.io/")
+qr.add_data("https://shahhussainsa.github.io/") #Enter URL 
 qr.make(fit=True)
 img = qr.make_image(fill_color="blue", back_color="yellow")
 img.save("Portfolio.png")
