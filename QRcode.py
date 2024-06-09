5c21bf8f222dba7c3703dcfd3f2c8791312ce4ee
SHAH HUSSAIN AHAMED S A <112507587+shahhussainsa@users.noreply.github.com>
2024-06-09 09:07:52 +0530

    qrcode

---
+++ QRcode.py
@@ -0,0 +1,11 @@
+import qrcode
+from PIL import Image
+
+qr = qrcode.QRCode(version = 1,
+                   error_correction = qrcode.constants.ERROR_CORRECT_H,
+                   box_size=10, border=4,)
+
+qr.add_data("https://shahhussainsa.github.io/")
+qr.make(fit=True)
+img = qr.make_image(fill_color="blue", back_color="yellow")
+img.save("Portfolio.png")
